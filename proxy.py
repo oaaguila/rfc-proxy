8404135ebd96c75f6271516252b83be96fde0bdc
oaaguila <58473999+oaaguila@users.noreply.github.com>
2026-05-11 21:30:15 -0600

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -8,15 +9,21 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 app = Flask(__name__)
 CORS(app)
 
-API_KEY = "b5a3ddc5-7da1f41d"
+API_URL = "https://3.129.253.159/api/RFC/consulta_rfc"
+API_KEY = "65e84be33532fb784c48129675f9eff3a682b27168c0ea744b2cf58ee02337c5"
 
 @app.route("/consulta_rfc/<rfc>", methods=["GET"])
 def consulta_rfc(rfc):
     rfc = rfc.strip().upper()
     try:
-        resp = requests.get(
-            f"https://satpi.mx/api/search/{rfc}",
-            headers={"x-api-key": API_KEY},
+        resp = requests.post(
+            API_URL,
+            data=json.dumps({"apikey": API_KEY, "rfc": rfc}),
+            headers={
+                "Content-Type": "application/json",
+                "accept": "text/plain"
+            },
+            verify=False,
             timeout=10
         )
         try:
